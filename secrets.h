71b9aafffb7e19f7b6b80c946b1b2bf9a1d9b83b
Timothy Tran <thtran5@uci.edu>
2026-03-11 03:46:22 -0700

    secrets file template

---
+++ secrets.h
@@ -0,0 +1,19 @@
+// =============================================================================
+//  Save this file in the SAME folder as the .ino sketch.
+// =============================================================================
+
+#ifndef SECRETS_H
+#define SECRETS_H
+
+// User config
+#define BLYNK_TEMPLATE_ID   "TMPL_XXXXXXXXXX"
+#define BLYNK_TEMPLATE_NAME "SP500 Tracker"
+#define BLYNK_AUTH_TOKEN    "your_blynk_auth_token_here"
+#define WIFI_SSID           "your_wifi_ssid_here"
+#define WIFI_PASS           "your_wifi_password_here"
+
+// constants
+#define BLYNK_HOST          "blynk.cloud"
+#define BLYNK_PORT          80
+
+#endif // SECRETS_H
